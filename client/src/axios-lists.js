7fcffe54e06756dab577af426242558140adee42
Niraj Mehta <1nt18is197.niraj@nmit.ac.in>
2021-07-01 15:46:10 +0530

    first commit

---
+++ client/src/axios-lists.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+let serverURL = 'http://localhost:8081';
+
+if(process.env.NODE_ENV === 'production'){
+    serverURL = 'https://findsport2play.herokuapp.com/';
+}
+
+const instance = axios.create({
+   baseURL: serverURL,
+   headers:{
+    "Authorization":localStorage.getItem("jwtToken")
+   }
+});
+
+export default instance;
